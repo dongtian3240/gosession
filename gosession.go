f79751fae6c6100a8ae93d10bfbea8d84be19319
dongtian3240 <dongtian@dongtiandeMacBook-Pro.local>
2016-03-23 23:38:42 +0800

    提交session验证有效期问题

--- gosession.go
+++ gosession.go
@@ -3,6 +3,7 @@ package gosession
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
 	"net/http"
 	"sync"
 	"time"
@@ -16,6 +17,9 @@ var (
 	GoSessionId string = "gosession-id"
 )
 
+type SyncGoSession interface {
+	UpdateSession(response http.ResponseWriter, request *http.Request)
+}
 type GoSession struct {
 	SId  string
 	data map[string]interface{}
@@ -33,9 +37,13 @@ type GoSessionManager struct {
 	sync.Mutex
 }
 
-func NewGoSessionManager(gosessionsId string) *GoSessionManager {
+func NewGoSessionManager(gosessionsId string, gcInterval time.Duration) *GoSessionManager {
 	if gosessionsId == "" {
 		gosessionsId = GoSessionId
+	}
+
+	if gcInterval <= 0 {
+		gcInterval = time.Millisecond * 100
 	}
 	return &GoSessionManager{
 		gosessions:  make(map[string]*GoSession),
@@ -45,7 +53,7 @@ func NewGoSessionManager(gosessionsId string) *GoSessionManager {
 		HttpOnly:    HttpOnly,
 		Path:        Path,
 		GoSessionId: gosessionsId,
-		GCInterval:  time.Millisecond * 10,
+		GCInterval:  gcInterval,
 	}
 }
 
@@ -82,7 +90,7 @@ func (gsm *GoSessionManager) hasGoSession(key string) (*GoSession, bool) {
 //创建一个新的gosession
 func (gsm *GoSessionManager) createGoSession(response http.ResponseWriter, request *http.Request) *GoSession {
 
-	var bys = make([]byte, 512)
+	var bys = make([]byte, 32)
 	rand.Read(bys)
 	cValue := base64.StdEncoding.EncodeToString(bys)
 
@@ -101,7 +109,7 @@ func (gsm *GoSessionManager) createGoSession(response http.ResponseWriter, reque
 		data: make(map[string]interface{}),
 	}
 	gsm.gosessions[cValue] = goSession
-	gsm.CreateAts[cValue] = time.Now().Add(time.Duration(gsm.MaxAge))
+	gsm.CreateAts[cValue] = time.Now().Add(time.Second * time.Duration(gsm.MaxAge))
 
 	return goSession
 }
@@ -130,11 +138,56 @@ func (gs *GoSession) Delete(key string) {
 //垃圾收集处理过期的 GoSession
 func StartGC(gsm *GoSessionManager) {
 
-	//
-	//
-
 	go func() {
 
+		for _ = range time.Tick(gsm.GCInterval) {
+			fmt.Println("************ GC **************")
+			for k, v := range gsm.CreateAts {
+
+				if gsm.expiredSession(v) == false {
+
+					gsm.Delete(k)
+				}
+			}
+		}
 	}()
+
+}
+
+//判断是否过期
+func (gsm *GoSessionManager) expiredSession(at time.Time) bool {
+
+	return time.Now().Before(at)
+}
+
+//根据key删除gosession
+func (gsm *GoSessionManager) Delete(key string) {
+	gsm.Lock()
+	defer gsm.Unlock()
+
+	delete(gsm.CreateAts, key)
+	delete(gsm.gosessions, key)
+
+}
+
+// 更新gosession
+func (gsm *GoSessionManager) UpdateGoSession(response http.ResponseWriter, request *http.Request) {
+	currentCookie, err := request.Cookie(gsm.GoSessionId)
+
+	var key string
+	if err != nil || currentCookie == nil {
+		return
+	}
+
+	key = currentCookie.Value
+	_, ok := gsm.CreateAts[key]
+	if ok {
+		fmt.Println("=======update goSession ===========")
+		gsm.CreateAts[key] = time.Now().Add(time.Second * time.Duration(gsm.MaxAge))
+		fmt.Println("at ", gsm.CreateAts[key], "key=", key)
+		currentCookie.MaxAge = gsm.MaxAge
+
+		http.SetCookie(response, currentCookie)
+	}
 
 }
